474826beca77dfcd3d8d3dbdab368941f9167416
Andrey Silichev <silichev.rus94@gmail.com>
2022-03-20 23:22:13 +0300

    create simple arraylist

---
+++ SimpleArray.java
@@ -0,0 +1,15 @@
+package gittest;
+
+import java.util.ArrayList;
+
+public class SimpleArray {
+    public static void main(String[] args) {
+        ArrayList <String> list = new ArrayList<>();
+
+        list.add("first element");
+        list.add("second element");
+        list.add("third element");
+
+        System.out.println(list.toString());
+    }
+}
